1effe14c18d54ebf8a3b162e2c2ac3a14e785db1
Vũ Đức Trường <21520507@gm.uit.edu.vn>
2025-07-09 11:06:26 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ module "keyvault" {
   source              = "./modules/keyvault"
   resource_group_name = azurerm_resource_group.main.name
   location            = var.location
-  prefix              = "${var.prefix}-sea"
+  prefix              = "${var.prefix}"
   tenant_id           = data.azurerm_client_config.current.tenant_id
   secrets_file_content = file("${path.module}/secrets.yaml")
   object_id = data.azurerm_client_config.current.object_id
